c888f1560dc6e40ae5d229a0e1ace692342c1e13
WORD559 <josh.barrass.work@gmail.com>
2017-11-24 21:58:59 +0000

    Added permissions

--- tbww.py
+++ tbww.py
@@ -3,8 +3,26 @@
 import os
 from telegram.ext import Updater, CommandHandler
 
+class immutableDict(dict):
+    def __delattr__(self,*args,**kwargs):
+        return None
+    def __delitem__(self,*args,**kwargs):
+        return None
+    def __setattr__(self,*args,**kwargs):
+        return None
+    def __setitem__(self,*args,**kwargs):
+        return None
+    def update(self,*args,**kwargs):
+        return None
+    def pop(self,*args,**kwargs):
+        return None
+    def popitem(self,*args,**kwargs):
+        return None
+    def clear(self,*args,**kwargs):
+        return None
+
 class Bot(object):
-    def __init__(self,TOKEN,IP="0.0.0.0",PORT=None):
+    def __init__(self,TOKEN,IP="0.0.0.0",PORT=None,default_perms=float("inf")):
         """Leave PORT as None to autodetect for Heroku"""
         #set up basics and webserver config
         self.TOKEN = TOKEN
@@ -17,6 +35,10 @@ class Bot(object):
         self.updater = Updater(token=TOKEN)
         self.dispatcher = self.updater.dispatcher # Shortcut
 
+        #Set up permissions
+        self.permissions = None # This should be an immutableDict. It can still be overwritten but it should be fairly safe.
+        self.default_perms = defaukt_perms
+
     def start_webhook(self,host):
         """Host should be heroku app domain if on heroku"""
         self.updater.start_webhook(listen=self.IP,
@@ -25,11 +47,23 @@ class Bot(object):
         self.updater.bot.set_webhook(host+self.TOKEN)
         self.updater.idle()
 
-    def command(self,name):
+    def command(self,name,permissions=None):
         def decorator(function):
-            def wrapper():
-                return function
-            self.dispatcher.add_handler(CommandHandler(name,function))
+            def wrapper(*args,**kwargs):
+                if permissions != None:
+                    if permissions < self.permissions[args[1].message.from_user.id]:
+                        args[0].send_message(chat_id=args[1].message.chat_id,
+                                             text="You do not have permission to use this command!")
+                        return None
+                    else:
+                        return function()
+                return function()
+            
+            self.dispatcher.add_handler(CommandHandler(name,wrapper))
             
             return wrapper()
         return decorator
+
+    def set_permissions(self,D):
+        """Set an immutableDict of permissions"""
+        self.permissions = immutableDict(D)
