0eade9461be33c278fac80fdc1f57f36cda26278
WORD559 <josh.barrass.work@gmail.com>
2017-12-16 21:13:14 +0000

    Added a way to add a document handler via decorators

--- tbww.py
+++ tbww.py
@@ -1,7 +1,7 @@
 ##TBWW, based on python-telegram-bot
 
 import os
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, filters
 
 class immutableDict(dict):
     def __delattr__(self,*args,**kwargs):
@@ -84,3 +84,27 @@ class Bot(object):
             self.dispatcher.add_handler(CommandHandler(name,top(function),pass_args=pass_args))
         return decorator
 
+    def document_handler(self,permissions=None):
+        def decorator(function):
+            def top(function):
+                def wrapper(*args,**kwargs):
+                    if permissions != None:
+                        if (self.permissions.has_key(args[1].message.from_user.id) and permissions >= self.permissions[args[1].message.from_user.id]):
+                            function(*args,**kwargs)
+                            return None
+                        remote_perms = self.get_remote_permissions()
+                        if (remote_perms.has_key(args[1].message.from_user.id) and permissions >= remote_perms[args[1].message.from_user.id]):
+                            function(*args,**kwargs)
+                            return None
+                        if permissions >= self.default_perms:
+                            function(*args,**kwargs)
+                            return None
+                        args[0].send_message(chat_id=args[1].message.chat_id,
+                                                 text="You do not have permission to use this command!")
+                        return None
+                    else:
+                        function(*args,**kwargs)
+                
+                return wrapper
+            self.dispatcher.add_handler(MessageHandler(filters.Filters.document,top(function)))
+        return decorator
