bcf907e7cd9bbaf3f35ac6298b135eb888b8e1bf
WORD559 <josh.barrass.work@gmail.com>
2018-06-24 23:49:52 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="TBWW",
-    version="0.2.9",
+    version="0.2.10",
     description="Telegram Bot Wrapper Wraper",
     license="GNU GPL 3.0",
     install_requires=["python-telegram-bot"],
